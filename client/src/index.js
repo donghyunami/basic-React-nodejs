6f29ad5767ffb098ab8398b06e6e8c88852d13b9
leedong2897 <skyblue5030@naver.com>
2021-09-02 20:59:12 +0900

    리덕스 세팅

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { Provider } from 'react-redux';
 import 'antd/dist/antd.css';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
+
+const createStroeWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk) (createStore);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider
+      store={createStroeWithMiddleware(
+        Reducer,
+         window.__REDUX_DEVTOOLS_EXTENSION__&&
+         window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
+    >
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
