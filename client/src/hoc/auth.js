369811bce579389308f5d51a7a3d7a3bcd287219
leedong2897 <skyblue5030@naver.com>
2021-09-03 21:41:03 +0900

    최종

--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -2,28 +2,34 @@ import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { auth } from '../_actions/user_action';
 
-export default function auth_ (SpecificComponent, option, adminRoute = null) {
-    function AuthenticaionCheck(props) {
-        const dispatch = useDispatch();
-        useEffect(() => {
-            dispatch(auth().then(res => {
-                console.log(res)
-            }))
-         }, [])
-    }
-
-    return AuthenticaionCheck;
+export default function auth_(SpecificComponent, option, adminRoute = null) {
+  function AuthenticaionCheck(props) {
+    const dispatch = useDispatch();
+    useEffect(() => {
+      dispatch(auth()).then(res => {
+        //로그인 하지 않은 상태
+        if (!res.payload.isAuth) {
+            props.history.push('/login');
+          if (option) {
+            props.history.push('/login');
+          }
+        } else {
+          //로그인 한 상태
+          if (adminRoute && !res.payload.isAuth) {
+            props.history.push('/');
+          } else {
+            if (option === false) {
+              /* 
+                로그인한 유저가 들어갈 수 없는 
+                페이지에 접근하는 경우
+              */
+              props.history.push('/');
+            }
+          }
+        }
+      });
+    }, []);
+    return <SpecificComponent/>;
+  }
+  return AuthenticaionCheck;
 }
-
-/* 
-  SpecificComponent: 각 컴포넌트를 인자로 받음
-
-   option: 인증 체크 부분, null, true, false의 인자를 받음
-   - null: 아무나 출입이 가능한 페이지
-   - true: 로그인한 유저만 출입이 가능한 페이지
-   - false: 로그인한 유저는 출입 불가능한 페이지
-
-   adminRoute: admin 유저인지 판단, true의 인자를 받음 
-    - 기본값: null
-    - true: admin 유저만 출입가능한 페이지
-*/
